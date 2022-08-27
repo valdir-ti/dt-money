dd1bfb11e238dac274576436ce1bcb62323004e9
Valdir Silva <valdir.ti@gmail.com>
2022-08-26 23:48:37 -0300

    feat: add styles to modal and form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
 
+import { Container } from './styles';
+
+import CloseIcon from '../../assets/close.svg';
+
 Modal.setAppElement('#root');
 
 interface NewTransactionModalProps {
@@ -13,8 +17,36 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className='react-modal-content'
         >
-            <h2>Cadastrar transação</h2>
+            <button 
+                type='button' 
+                onClick={onRequestClose}
+                className='react-modal-close'
+            >
+                <img src={CloseIcon} alt="Close modal" />
+            </button>
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input 
+                    type="text" 
+                    placeholder='Título'
+                />
+
+                <input 
+                    type="number" 
+                    placeholder='Valor'
+                />
+                
+                <input 
+                    type="text" 
+                    placeholder='Categoria'
+                />
+
+                <button type="submit">Cadastrar</button>
+            </Container>
         </Modal>
     )
 }
