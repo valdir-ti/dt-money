d14729c223d6b6437bb61811381f4ab59fc4dc68
Valdir Silva <valdir.ti@gmail.com>
2022-08-27 17:03:17 -0300

    feat: add env variable

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -8,33 +8,35 @@ export function TransactionsTable() {
 
     return (
         <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {transactions.map(transaction => (
-                        <tr key={transaction.id}>
-                            <td>{transaction.title}</td>
-                            <td className={transaction.type}>
-                                {new Intl.NumberFormat('pt-BR', {
-                                    style: 'currency',
-                                    currency: 'BRL',                                    
-                                }).format(transaction.amount)}
-                            </td>
-                            <td>{transaction.category}</td>
-                            <td>
-                            {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
-                            </td>
-                        </tr>                    
-                    ))}
-                </tbody>
-            </table>
+            {transactions.length ?
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Valor</th>
+                            <th>Categoria</th>
+                            <th>Data</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {transactions.map(transaction => (
+                            <tr key={transaction.id}>
+                                <td>{transaction.title}</td>
+                                <td className={transaction.type}>
+                                    {new Intl.NumberFormat('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL',                                    
+                                    }).format(transaction.amount)}
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>
+                                {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                                </td>
+                            </tr>                    
+                        ))}
+                    </tbody>
+                </table> : <></>
+            }
         </Container>
     )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const appUrl = process.env.REACT_APP_ENVIRONMENT_MODE === 'local' ? 'http://localhost:3000/api' : 'https://dt-money-gules.vercel.app/api';
+
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: appUrl
 })
